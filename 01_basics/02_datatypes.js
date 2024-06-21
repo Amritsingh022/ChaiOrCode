dec1328ae1ff811f969b264d6526ade9d5e707ee
Amrit_22 <131852907+Amritsingh022@users.noreply.github.com>
2024-06-21 19:56:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all code as a newer version
+
+// alert(3+3) using nodejs,not browser
+console.log(3+3)
+console.log("hitesh")
+// documentation (tce9,mdn)
+
+let name="hitesh"
+let age=20
+let isLoggedIn=false
+let state;
+// range of a numbers =>2 to the power of 53
+//bigint used for a large numbers
+// string=>""
+//boolean=>true or false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof undefined)  //output->undefined
+console.log(typeof null)        //output->object
+
+
