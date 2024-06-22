b1ceaf7fd80b49ff285e543f453abd3d4e3815ef
Amrit_22 <131852907+Amritsingh022@users.noreply.github.com>
2024-06-22 22:47:46 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score="Amrit"
+  
+//const {score}=req.body
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber=Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// when convert "33"=>33  (gets easily convert)
+// "33abc" =>NaN
+// true =>1; false=>0
+// "" =>false 
+//"Amrit"=>true
+/*let isLoggedIn="Amrit"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+*/
+
+let someNumber=90
+let stringnumber=String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringnumber)
