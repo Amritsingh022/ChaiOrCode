4ef2b55b179231eecaa98ff363520956ba06e7d4
Amrit_22 <131852907+Amritsingh022@users.noreply.github.com>
2024-06-26 19:53:21 +0000

    datatypesummary

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,13 +2,14 @@
 
 // alert(3+3) using nodejs,not browser
 console.log(3+3)
-console.log("hitesh")
+//console.log("hitesh")
 // documentation (tce9,mdn)
 
 let name="hitesh"
 let age=20
 let isLoggedIn=false
 let state;
+let amr="amrit"
 // range of a numbers =>2 to the power of 53
 //bigint used for a large numbers
 // string=>""
@@ -16,10 +17,9 @@ let state;
 //null=>standalone value
 //undefined=>
 //symbol=>unique
-
 //object
 
 console.log(typeof undefined)  //output->undefined
 console.log(typeof null)        //output->object
-
-
+console.log(amr)
+console.log(name,age,stage,amr)
--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,4 @@
-let score="Amrit"
+//let score="Amrit"
   
 //const {score}=req.body
 //console.log(typeof score);
@@ -17,7 +17,36 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
 */
 
+/*
 let someNumber=90
 let stringnumber=String(someNumber)
 console.log(stringnumber);
 console.log(typeof stringnumber)
+*/ 
+
+/*let value=3;
+let negvalue=-3;
+console.log(negvalue);
+*/
+
+//******************operations*********************
+
+let value=3
+let negvalue=-value;
+//console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2);
+//console.log(2**3);     // operation used for the power
+
+/*let str1="Amrit";
+let str2="Varun";
+let str3=str1+str2;
+console.log(str3); */
+console.log("1"+2);
+console.log(1+2+3+"4");
+console.log("1"+"2"+"3"+4);
+console.log("1"+2+"3"+4);
