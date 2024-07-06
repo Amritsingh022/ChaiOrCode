dfd57e532c7bf15b443b5166487bb733d813b3a4
Amrit_22 <131852907+Amritsingh022@users.noreply.github.com>
2024-07-06 19:56:31 +0000

    nums and  maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+//const score=400; 
+
+//const balance =new Number("100")
+//console.log(balance)
+
+//console.log(balance.toString())
+//console.log(balance.toFixed(2))
+
+//const otherNumber=123.8966
+
+//console.log(otherNumber.toPrecision(4))
+
+//const hundreds=100000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+/*************        MATHS       ****************/ 
+
+//console.log(Math)
+//console.log(Math.abs(-5)) 
+//console.log(Math.round(4.6))
+//console.log(Math.ceil(4.6))        //upgrade the value 
+//console.log(Math.floor(4.9))       // value will be the same 
+//console.log(Math.max(4,5,9,6,48))
+//console.log(Math.min(44,65,45,94,25,72))
+
+console.log(Math.random());    //values lies between 0 and 1
+console.log((Math.random()*10)+1)  
+console.log(Math.floor(Math.random()*10)+1)
+
+const min=10;
+const max=20;
+
+console.log(Math.floor((Math.random())*(max-min+1))+min)
